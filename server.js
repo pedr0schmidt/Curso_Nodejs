b37155fcbb2ab533203481d47a54d1b6bb330bc8
pedr0schmidt <pedrosschmidt@outlook.com>
2020-01-27 16:17:43 -0300

    restante dos arquivos

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const http = require('http');
+const app = require('./app');
+
 
 const port = process.env.PORT || 3000;
+//setando uma porta padrão
+const server = http.createServer(app); //crinado server
+server.listen(port);
+
+
+//passando para o server todo conteúdo exportado de app
