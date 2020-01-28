60e4836358dffe0eeb26f1257b018d17dd47d4cd
pedr0schmidt <pedrosschmidt@outlook.com>
2020-01-28 09:53:42 -0300

    Terceira aula

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+app.use(morgan('dev'));
+//O morgan monitora toda a execução e nos retorna os logs
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+
+//Caso a rota não seja encontrada 
+app.use((req, res, next) => {
+  const erro = new Error('Não encontrado');
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+      erro: {
+        mensagem: error.message
+      }
+    });
+});
 
 
 
